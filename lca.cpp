6a6d02911b8e25a8eed84229732a6af301a8ff47
StupidGuy13 <83475876+StupidGuy13@users.noreply.github.com>
2021-06-24 23:03:49 +0400

    Update lca.cpp

--- lca.cpp
+++ lca.cpp
@@ -5,75 +5,75 @@
 #define pii pair<int,int>
 #define pll pair<ll,ll>
 #define ull unsigned ll
-#define pb push_back
 #define epb emplace_back
+#define pb push_back
 #define inf 1e9+1
 #define linf 1e18+1
 using namespace std;
-//finding lowest common ancestor in O(logn) with O(nlogn) preparation
-vector<vector<int> >g(200001);
-int P[200001][21];
-vector<int>d(200001);
-int n;
-
-void dfs(int v,int e=-1){
-    P[v][0]=e;
+const int NMAX=200001;
+int n,q;
+void dfs(int v,int e=-1);
+vector<vector<int> >g(NMAX);
+vector<int>dp(NMAX,0);
+int a[NMAX][20];
+void dfs(int v,int e){
+    a[v][0]=e;
     for(int to:g[v]){
-        if(to==e)continue;
-        d[to]=d[v]+1;
-        dfs(to,v);
+        if(to!=e)dp[to]=dp[v]+1,dfs(to,v);
     }
 }
-
+ 
 void prepare(){
-    for(int i=1;i<=n;i++){
-        for(int j=0;j<20;j++)P[i][j]=-1;
-    }
     dfs(1);
+    for(int i=1;i<=19;i++){
+        for(int j=1;j<=n;j++)a[j][i]=-1;
+    }
     for(int j=1;j<20;j++){
         for(int i=1;i<=n;i++){
-            if(P[i][j-1]!=-1)
-            P[i][j]=P[P[i][j-1]][j-1];
+            if(a[i][j-1]!=-1)a[i][j]=a[a[i][j-1]][j-1];
         }
     }
 }
-
-int lca(int u,int v){
-    if(d[u]<d[v])swap(u,v);
-    int k=d[u]-d[v];
-    while(k){
-        int t=log2(k);
-        k-=(1<<t);
-        u=P[u][t];
+ 
+int binary_lift(int v,int k){
+    for(int j=0;j<20;j++){
+        if(k&(1<<j))v=a[v][j];
+        if(v==-1)break;
     }
+    return v;
+}
+ 
+int lca(int v,int u){
+    if(dp[v]>dp[u])swap(u,v);
+    int x=dp[u]-dp[v];
+    u=binary_lift(u,x);
     if(u==v)return v;
     for(int j=19;j>=0;j--){
-        if(P[u][j]!=P[v][j]){
-            u=P[u][j];
-            v=P[v][j];
+        if(a[u][j]!=a[v][j]){
+            v=a[v][j];
+            u=a[u][j];
         }
     }
-    return P[v][0];
+    return a[u][0];
 }
-
+ 
 void solve(){
-    cin>>n;
-    for(int i=1;i<=n-1;i++){
-        int u,v;cin>>u>>v;
-        g[u].pb(v);
-        g[v].pb(u);
+    cin>>n>>q;
+    for(int i=2;i<=n;i++){
+        int x;cin>>x;
+        g[x].pb(i);
+        g[i].pb(x);
     }
     prepare();
-    int q;cin>>q;
     while(q--){
-        int u,v;cin>>u>>v;
+        int u,v;
+        cin>>u>>v;
         cout<<lca(u,v)<<"\n";
     }
 }
-
+ 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
     solve();
+    return 0;
 }
