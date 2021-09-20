de4f9f7957fde32b2522b5f63e6f2850e7eeb4d7
StupidGuy13 <83475876+StupidGuy13@users.noreply.github.com>
2021-09-20 23:49:55 +0400

    Update sieve.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -35,7 +35,6 @@ void solve(){
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-    freopen("in.in","r",stdin);freopen("out.out","w",stdout);
     int test=1;//cin>>test;
     while(test--){
         solve();
